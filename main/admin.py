c993e803493f852dc82daeb1a4c7790c02585804
amirustop777 <amirustop777@gmail.com>
2026-05-23 18:10:42 +0500

    закомментировал

--- main/admin.py
+++ main/admin.py
@@ -1,17 +1,22 @@
 from django.contrib import admin
 from .models import Category, Product
-# Register your models here.
+
+# регистрация моделей в админ-панели
 
 @admin.register(Category)
-class  CategoryAdmin(admin.ModelAdmin):
-    list_display = ['name','slug']
+class CategoryAdmin(admin.ModelAdmin):
+    # колонки, которые будут видны в списке категорий
+    list_display = ['name', 'slug']
+    # автоматическое заполнение слага на основе имени при вводе
     prepopulated_fields = {'slug': ('name', )}
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['name', 'category', 'price', 'available',
-                    'created', 'updated']
+    # колонки, которые будут видны в списке товаров
+    list_display = ['name', 'category', 'price', 'available', 'created', 'updated']
+    # правая панель с фильтрами для быстрой сортировки
     list_filter = ['available', 'created', 'updated', 'category']
+    # поля, которые можно редактировать прямо в списке, не заходя в товар
     list_editable = ['price', 'available']
+    # автоматическое заполнение слага для товара
     prepopulated_fields = {'slug': ('name', )}
-    
